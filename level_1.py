04eca82db67dd67bef9aa9c9fc7c4c1365cf089d
v1rus41l <romanmityakin2006@mail.ru>
2023-01-03 21:39:50 +0300

    animation_of_knight

--- level_1.py
+++ level_1.py
@@ -3,6 +3,7 @@ import sys
 
 import pygame
 
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
@@ -10,18 +11,21 @@ def load_image(name, colorkey=None):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
-    # if colorkey is not None:
-    #     image = image.convert()
-    #     if colorkey == -1:
-    #         colorkey = image.get_at((0, 0))
-    #     image.set_colorkey(colorkey)
-    # else:
-    #     image = image.convert_alpha()
-    # return image
-
 WIDTH, HEIGHT = 800, 600
+
+knight_img = [pygame.image.load('data/3.png'),
+              pygame.image.load('data/4.png'), pygame.image.load('data/5.png'), pygame.image.load('data/6.png'),
+              pygame.image.load('data/5.png'), pygame.image.load('data/4.png')]
+img_count = 0
 
 FPS = 50
 
@@ -32,12 +36,49 @@ clock = pygame.time.Clock()
 
 player = None
 
+class Knight(pygame.sprite.Sprite):
+    image = load_image("1.png")
+
+    def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(*group)
+        self.image = Knight.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 800
+        self.rect.y = 50
+
+    def staying(self, vector):
+        if vector:
+            screen.blit(pygame.image.load('data/1.png'), (self.rect.x, self.rect.y))
+        else:
+            screen.blit(pygame.transform.flip(pygame.image.load('data/1.png'), True, False), (self.rect.x, self.rect.y))
+
+    def animation(self, vector):
+        global img_count
+        if img_count == 30:
+            img_count = 0
+        if vector:
+            screen.blit(knight_img[img_count // 5], (self.rect.x, self.rect.y))
+        else:
+            screen.blit(pygame.transform.flip(knight_img[img_count // 5], True, False), (self.rect.x, self.rect.y))
+        img_count += 1
+        clock.tick(80)
+
+    def jumping(self):
+        self.rect.y -= 5 * clock.tick(60)
+        self.rect.y += 15 * clock.tick(60)
+
+
+
+
 # группы спрайтов
 def main():
     pygame.init()
     size = width, height = 850, 450
     screen = pygame.display.set_mode(size)
     all_sprites = pygame.sprite.Group()
+    knight = Knight()
     arrow_image = load_image("1.png")
     arrow_image = pygame.transform.scale(arrow_image, (100, 100))
     cursor = pygame.sprite.Sprite(all_sprites)
@@ -46,21 +87,29 @@ def main():
     fon = pygame.transform.scale(load_image('level1_fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     running = True
+    vector = False
     while running:
+        action = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                cursor.rect.x = cursor.rect.x + 10
-            if pygame.key.get_pressed()[pygame.K_LEFT]:
-                cursor.rect.x = cursor.rect.x - 10
-            if pygame.key.get_pressed()[pygame.K_UP]:
-                cursor.rect.y = cursor.rect.y - 10
-            if pygame.key.get_pressed()[pygame.K_DOWN]:
-                cursor.rect.y = cursor.rect.y + 10
-
         screen.blit(fon, (0, 0))
-        all_sprites.draw(screen)
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            knight.rect.x += 2
+            vector = True
+            Knight.animation(knight, vector)
+            action = True
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            knight.rect.x -= 2
+            vector = False
+            Knight.animation(knight, vector)
+            action = True
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            Knight.jumping(knight)
+        if not action:
+            Knight.staying(knight, vector)
+        # screen.blit(pygame.image.load('data/1.png'), (0, 0))
+        # all_sprites.draw(screen)
         pygame.display.flip()
     pygame.quit()
 
--- main_character.py
+++ main_character.py
@@ -1,3 +1,33 @@
-class Knight():
-    def __init__(self):
-        pass
+import os
+import random
+import sys
+
+import pygame
+from level_1 import  img_count
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Knight(pygame.sprite.Sprite):
+    image = load_image("1.png")
+
+    def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(*group)
+        self.image = Knight.image
+        self.rect = self.image.get_rect()
+
+    def animation(self):
+        global img_count
+        if img_count == 6:
+            img_count = 0
+        screen
